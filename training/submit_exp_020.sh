ec7610704c1e917ee3f67867ca24d1cca47826ff
warriors44 <shinya.tsc7@gmail.com>
2026-03-16 23:07:52 +0900

    Update training scripts and .gitignore for improved configuration and output management
    - Modified `.gitignore` to include new output directories and shell scripts for better file management.
    - Updated `submit_exp_020.sh` to change output paths and add new training parameters for enhanced model training.
    - Revised `submit_exp_lo.sh` to adjust resource allocation, update output paths, and include additional training options for improved performance and debugging capabilities.

--- training/submit_exp_020.sh
+++ training/submit_exp_020.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+#PBS -l select=1:ncpus=16:ngpus=1
+#PBS -q week
+#PBS -l walltime=168:00:00
 
-#SBATCH -p gpu
-#SBATCH --mem=128g
-#SBATCH --gres=gpu:a100:1
-#SBATCH -c 12
-#SBATCH -t 7-00:00:00
-#SBATCH --output=exp_020.out
+cd $PBS_O_WORKDIR
+source ../.venv/bin/activate
 
-source activate mlfold-test
 python ./training.py \
-           --path_for_outputs "./exp_020" \
-           --path_for_training_data "path_to/pdb_2021aug02" \
+           --path_for_outputs "./exp_020_sample" \
+           --path_for_training_data ../data/pdb_2021aug02_sample \
            --num_examples_per_epoch 1000 \
-           --save_model_every_n_epochs 50
+           --save_model_every_n_epochs 50 \
+           --max_protein_length 4000 \
+           --batch_size 4000 \
+           --num_epochs 1000
--- training_lo/submit_exp_lo.sh
+++ training_lo/submit_exp_lo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=8:ngpus=1
+#PBS -l select=1:ncpus=16:ngpus=1
 #PBS -q week
 #PBS -l walltime=168:00:00
 
@@ -7,13 +7,17 @@
 cd $PBS_O_WORKDIR
 source ../.venv/bin/activate
 
-python3 ./training_lo.py \
-  --path_for_outputs ./exp_lo/ \
-  --path_for_training_data ../data/pdb_2021aug02_sample \
-  --num_examples_per_epoch 1000 \
-  --save_model_every_n_epochs 50 \
-  --num_lo_samples 2 \
-  --eval_mode is_q \
+python3 ./training_lo_debug.py \
+  --path_for_outputs ./exp_lo \
+  --path_for_training_data ../data/pdb_2021aug02 \
+  --save_model_every_n_epochs 10 \
   --eval_num_samples 8 \
-  --separate_q_decoder 0
-
+  --separate_q_decoder 0 \
+  --batch_size 4000 \
+  --num_epochs 1000 \
+  --num_lo_samples 8 \
+  --num_neighbors 48 \
+  --max_protein_length 4000 \
+  --debug_log_interval 1 \
+    # --previous_checkpoint ./exp_lo/model_weights/epoch_best_proxy.pt \
+    # --num_examples_per_epoch 1000 \
